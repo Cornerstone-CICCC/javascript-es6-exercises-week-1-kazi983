0dd883bf565b93c2a96f02ebb3f1f0bbb6867690
Kazi <kaoru.kajihara.mail@gmail.com>
2026-05-25 21:41:00 -0700

    exercise 20 completed

--- exercises/ex20.js
+++ exercises/ex20.js
@@ -9,3 +9,20 @@ var mercenary = "Jayne";
 var mechanic = "Kaylee";
 Then, print the crew array to the console.
 */
+
+const crew = [];
+var captain = 'Mal';
+var second = 'Zoe';
+var pilot = 'Wash';
+var companion = 'Inara';
+var mercenary = 'Jayne';
+var mechanic = 'Kaylee';
+
+crew.push(captain);
+crew.push(second);
+crew.push(pilot);
+crew.push(companion);
+crew.push(mercenary);
+crew.push(mechanic);
+
+console.log('crew :>> ', crew);
